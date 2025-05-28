6607a6f109aabf3bc30adf4df90c3cfb52ba5562
Afonso Pimenta <pimenta.afonsosilva@gmail.com>
2025-05-28 23:24:13 +0100

    feat: add click animation to movie selection

--- src/app/movie-card.tsx
+++ src/app/movie-card.tsx
@@ -8,7 +8,7 @@ export const MovieCard = ({ src }: { src: string }) => {
 
   return (
     <button
-      className="max-w-xs overflow-hidden rounded-lg shadow-xl transition-all duration-200 hover:scale-105"
+      className="max-w-xs overflow-hidden rounded-lg shadow-xl transition-all duration-200 hover:scale-105 focus-visible:scale-105 active:scale-103"
       onClick={router.refresh}
     >
       <Image src={src} alt="" width={1280} height={1920} />
